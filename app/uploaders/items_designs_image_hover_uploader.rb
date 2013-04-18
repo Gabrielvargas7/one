f2ea33d126c294b67f8cdd607a3c2f9ff1d08c3d
gabriel@mywebroom.com <gabriel@mywebroom.com>
2013-04-18 05:22:57 -0700

    set default images

--- app/uploaders/items_designs_image_hover_uploader.rb
+++ app/uploaders/items_designs_image_hover_uploader.rb
@@ -20,13 +20,14 @@ class ItemsDesignsImageHoverUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  #Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+     # For Rails 3.1+ asset pipeline compatibility:
+     # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+    "/images/fallback/items_design/default_items_design.png"
+     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
--- app/uploaders/themes_image_uploader.rb
+++ app/uploaders/themes_image_uploader.rb
@@ -20,13 +20,15 @@ class ThemesImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  #Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+     # For Rails 3.1+ asset pipeline compatibility:
+     # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+     "/images/fallback/themes/default_theme.png"
+
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
