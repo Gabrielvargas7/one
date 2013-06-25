285776536d95d9d26e24e8f59a883760f188fddd
gabriel@mywebroom.com <gabriel@mywebroom.com>
2013-06-25 13:18:07 -0700

    rspec items_controller ..

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -40,7 +40,7 @@ describe ItemsController do
   after(:all){ delete_init_data }
 
   before  do
-    @theme = FactoryGirl.create(:theme)
+    @item = FactoryGirl.create(:item)
     @admin = FactoryGirl.create(:admin)
     sign_in @admin
     #puts "Admin user signin cookie: "+cookies[:remember_token].to_s
@@ -48,7 +48,7 @@ describe ItemsController do
 
 
   #the (subject)line declare the variable that is use in all the test
-  subject { @theme }
+  subject { @item }
 
 
   #***********************************
@@ -58,11 +58,11 @@ describe ItemsController do
   describe "GET index",tag_index:true do
 
     context "is admin user" do
-      let(:themes_all) { Theme.all }
+      let(:item_all) { Item.all }
 
-      it "assigns all themes as @themes" do
+      it "assigns all themes as @item" do
         get :index
-        assigns(:themes).should eq(themes_all)
+        assigns(:items).should eq(item_all)
       end
 
       it "renders the :index view" do
@@ -93,20 +93,19 @@ describe ItemsController do
   # rspec test  show
   #***********************************
 
-
   describe "GET show", tag_show:true do
 
     context "is admin user" do
 
-      it "assigns the requested themes as @themes" do
-        get :show, id: @theme
-        assigns(:theme).should eq(@theme)
+      it "assigns the requested item as @item" do
+        get :show, id: @item
+        assigns(:item).should eq(@item)
 
       end
 
       it "renders the #show view" do
 
-        get :show, id: @theme
+        get :show, id: @item
         response.should render_template :show
       end
 
@@ -119,12 +118,12 @@ describe ItemsController do
       end
 
       it "redirect to root " do
-        get :show, id:@theme
+        get :show, id:@item
         response.should redirect_to root_path
       end
 
       it "not render to show " do
-        get :show, id:@theme
+        get :show, id:@item
         response.should_not render_template :show
       end
 
@@ -136,16 +135,15 @@ describe ItemsController do
   # rspec test  new
   #***********************************
 
-
   describe "GET new",tag_new:true do
 
     context "is admin user"  do
-      it "assigns a new themes as @themes" do
+      it "assigns a new item as @item" do
 
-        new_theme = FactoryGirl.create(:theme)
-        Theme.should_receive(:new).and_return(new_theme)
+        new_item = FactoryGirl.create(:item)
+        Item.should_receive(:new).and_return(new_item)
         get :new
-        assigns[:theme].should eq(new_theme)
+        assigns[:item].should eq(new_item)
       end
     end
     context "is not admin user"  do
@@ -171,11 +169,11 @@ describe ItemsController do
 
     context "is admin user"  do
 
-      it "assigns the requested themes as @themes" do
+      it "assigns the requested item as @item" do
 
-        new_theme = FactoryGirl.create(:theme)
-        get :edit, id: new_theme
-        assigns[:theme].should eq(new_theme)
+        new_item = FactoryGirl.create(:item)
+        get :edit, id: new_item
+        assigns[:item].should eq(new_item)
       end
     end
 
@@ -188,14 +186,12 @@ describe ItemsController do
       end
 
       it "redirect to root " do
-        new_theme = FactoryGirl.create(:theme)
-        get :edit, id: new_theme
+        new_item = FactoryGirl.create(:item)
+        get :edit, id: new_item
         response.should redirect_to root_path
       end
     end
-
   end
-
 
   #***********************************
   # rspec test create
@@ -207,24 +203,24 @@ describe ItemsController do
     describe "is admin user" do
       context "with valid params" do
 
-        it "creates a new Theme" do
+        it "creates a new item" do
 
           expect {
-            post :create,theme: FactoryGirl.attributes_for(:theme)
-          }.to change(Theme, :count).by(1)
+            post :create,item: FactoryGirl.attributes_for(:item)
+          }.to change(Item, :count).by(1)
 
 
         end
 
-        it "assigns a newly created themes as @themes" do
-          post :create,theme: FactoryGirl.attributes_for(:theme)
-          assigns(:theme).should be_a(Theme)
-          assigns(:theme).should be_persisted
+        it "assigns a newly created item as @item" do
+          post :create,item: FactoryGirl.attributes_for(:item)
+          assigns(:item).should be_a(Item)
+          assigns(:item).should be_persisted
         end
 
-        it "redirects to the created themes" do
-          post :create, theme: FactoryGirl.attributes_for(:theme)
-          response.should redirect_to(Theme.last)
+        it "redirects to the created item" do
+          post :create, item: FactoryGirl.attributes_for(:item)
+          response.should redirect_to(Item.last)
         end
       end
 
@@ -232,11 +228,11 @@ describe ItemsController do
 
         context "with invalid attributes" do
           it "does not save the new contact" do
-            expect{ post :create, theme: FactoryGirl.attributes_for(:theme,name:nil)
-            }.to_not change(Theme,:count)
+            expect{ post :create, item: FactoryGirl.attributes_for(:item,clickable:nil)
+            }.to_not change(Item,:count)
           end
           it "re-renders the new method" do
-            post :create, theme: FactoryGirl.attributes_for(:theme,name:nil)
+            post :create, item: FactoryGirl.attributes_for(:item,clickable:nil)
             response.should render_template :new
           end
         end
@@ -245,19 +241,21 @@ describe ItemsController do
 
     end
 
-    describe "is not admin user" do
+
+
+      describe "is not admin user" do
       before do
         @user  = FactoryGirl.create(:user)
         sign_in @user
       end
 
       it "redirects to root" do
-        post :create, theme: FactoryGirl.attributes_for(:theme)
+        post :create, item: FactoryGirl.attributes_for(:item)
         response.should redirect_to(root_path)
       end
       it "not redirects to the created themes" do
-        post :create, theme: FactoryGirl.attributes_for(:theme)
-        response.should_not redirect_to(Theme.last)
+        post :create, item: FactoryGirl.attributes_for(:item)
+        response.should_not redirect_to(Item.last)
       end
     end
 
@@ -274,38 +272,38 @@ describe ItemsController do
     describe "is admin user" do
 
       context "valid attributes" do
-        it "located the requested @theme" do
-          put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme)
-          assigns(:theme).should eq(@theme)
+        it "located the requested @item" do
+          put :update, id: @item, item: FactoryGirl.attributes_for(:item)
+          assigns(:item).should eq(@item)
         end
       end
 
-      it "changes @theme's attributes" do
-        put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme, name: "Larry", description: "Smith")
-        @theme.reload
-        @theme.name.should eq("Larry")
-        @theme.description.should eq("Smith")
+      it "changes @item's attributes" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:item, name: "my new item", clickable: "no")
+        @item.reload
+        @item.name.should eq("my new item")
+        @item.clickable.should eq("no")
       end
 
-      it "redirects to the updated theme" do
-        put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme)
-        response.should redirect_to @theme
+      it "redirects to the updated item" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:item)
+        response.should redirect_to @item
       end
 
       context "invalid attributes" do
 
-        it "locates the requested @theme" do
-          put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme,name:nil)
-          assigns(:theme).should eq(@theme)
+        it "locates the requested @item" do
+          put :update, id: @item, item: FactoryGirl.attributes_for(:item,clickable:nil)
+          assigns(:item).should eq(@item)
         end
-        it "does not change @theme's attributes" do
-          put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme, name:"John", description: "Smith")
-          @theme.reload
-          @theme.name.should_not eq("Larry")
-          @theme.description.should_not eq("Smith")
+        it "does not change @item's attributes" do
+          put :update, id: @item, item: FactoryGirl.attributes_for(:item, name:"my new item", clickable: "other")
+          @item.reload
+          @item.name.should_not eq("my new item")
+          @item.clickable.should_not eq("other")
         end
         it "re-renders the edit method" do
-          put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme,name:nil)
+          put :update, id: @item, item: FactoryGirl.attributes_for(:item,clickable:nil)
           response.should render_template :edit
         end
       end
@@ -318,108 +316,8 @@ describe ItemsController do
       end
 
       it "redirects to root " do
-        put :update, id: @theme, theme: FactoryGirl.attributes_for(:theme)
+        put :update, id: @item, item: FactoryGirl.attributes_for(:item)
         response.should redirect_to root_path
-      end
-
-    end
-
-
-  end
-
-  #***********************************
-  # rspec test  #json_index
-  #***********************************
-
-
-  describe "api #json_index",tag_json_index:true do
-
-    describe "is public api" do
-      before do
-        sign_out
-      end
-
-
-      it "should be successful" do
-        get :json_index, theme: @theme, :format => :json
-        response.should be_success
-      end
-      let(:themes_all){Theme.all}
-      it "should set theme" do
-        get :json_index, :format => :json
-        assigns(:themes).as_json.should == themes_all.as_json
-      end
-
-      it "has a 200 status code" do
-        get :json_index, theme: @theme, :format => :json
-        expect(response.status).to eq(200)
-      end
-
-      context "get all values " do
-        it "should return json_index theme in json" do # depend on what you return in action
-          get :json_index, :format => :json
-          body = JSON.parse(response.body)
-          #puts "body ---- > "+body.to_s
-          #puts "theme ----> "+@theme.as_json.to_s
-          #puts "body name ----> " + body[0]["name"].to_s
-          #puts "body image name ----> " + body[0]["image_name"]["url"].to_s
-          #puts "theme name----> "+@theme.name.to_s
-          #puts "theme image name----> "+@theme.image_name.to_s
-
-          body.each do |body_theme|
-            @theme_json = Theme.find(body_theme["id"])
-            body_theme["name"].should == @theme_json.name
-            body_theme["description"].should == @theme_json.description
-            body_theme["id"].should == @theme_json.id
-            body_theme["image_name"]["url"].should == @theme_json.image_name.to_s
-            body_theme["image_name_selection"]["url"].should == @theme_json.image_name_selection.to_s
-          end
-        end
-      end
-    end
-  end
-
-  #***********************************
-  # rspec test  #json_show
-  #***********************************
-
-
-  describe "api #json_show",tag_json_show:true do
-
-    describe "is public api" do
-      before do
-        sign_out
-      end
-
-
-      it "should be successful" do
-        get :json_show, id: @theme, :format => :json
-        response.should be_success
-      end
-
-      let(:themes_all){Theme.all}
-      it "should set theme" do
-        get :json_show,id:@theme, :format => :json
-        assigns(:theme).as_json.should == @theme.as_json
-      end
-
-      it "has a 200 status code" do
-        get :json_show, id: @theme, :format => :json
-        expect(response.status).to eq(200)
-      end
-
-      context "get all values " do
-        it "should return json_show theme in json" do # depend on what you return in action
-          get :json_show,id:@theme, :format => :json
-          body = JSON.parse(response.body)
-          body["name"].should == @theme.name
-          body["description"].should == @theme.description
-          body["id"].should == @theme.id
-          body["image_name"]["url"].should == @theme.image_name.to_s
-          body["image_name_selection"]["url"].should == @theme.image_name_selection.to_s
-
-
-        end
       end
     end
   end
