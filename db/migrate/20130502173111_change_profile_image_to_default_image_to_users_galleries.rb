7764a5a710eb84f977bc399524ca93f087c40e72
gabriel@mywebroom.com <gabriel@mywebroom.com>
2013-07-12 10:50:57 -0700

    server unicorn

--- db/migrate/20130502173111_change_profile_image_to_default_image_to_users_galleries.rb
+++
@@ -1,8 +0,0 @@
-class ChangeProfileImageToDefaultImageToUsersGalleries < ActiveRecord::Migration
-  def up
-    rename_column :users_galleries, :profile_image, :default_image
-  end
-
-  def down
-  end
-end
