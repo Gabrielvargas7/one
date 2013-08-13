0f9404e4b951924cbe99c9d264737fe41d1c915d
gabriel@mywebroom.com <gabriel@mywebroom.com>
2013-08-12 18:38:10 -0700

    remove temporal CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   # add this lines on the production server
   if Rails.env.production?
       #Force to signout to prevent CSRF attacks
-      def handle_unverified_request
-        sign_out
-        super
-      end
+      #def handle_unverified_request
+      #  sign_out
+      #  super
+      #end
   end
 
 
