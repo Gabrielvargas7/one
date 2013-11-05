a7c0da1ac7abcb002fee0dea0e30f54d3059eee5
rooms.team@mywebroom.com <rooms.team@mywebroom.com>
2013-11-04 18:22:55 -0800

    static content and boundle entire room

--- app/models/static_content.rb
+++ app/models/static_content.rb
@@ -1,3 +1,11 @@
 class StaticContent < ActiveRecord::Base
   attr_accessible :description, :image_name, :name
+
+  mount_uploader :image_name, StaticContentsImageNameUploader
+
+  validates :name,
+            presence:true,
+            uniqueness:{ case_sensitive: false }
+
+
 end
