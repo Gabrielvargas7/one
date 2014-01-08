054f057f57e10459fa4e255ca3b311e879576c22
gabriel <gabriel@mywebroom.com>
2014-01-08 14:14:45 -0800

    rspec capybarra test

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,11 +9,8 @@ end
 
 def sign_in(user)
   visit signin_path
-  #fill_in "Email",    with: user.email
-  #fill_in "Password", with: user.password
-  fill_in "Email",    with: 'gabriel@mywebroom.com'
-  fill_in "Password", with: 'Franc15c0'
-
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
 
   click_button "LOGIN"
   # Sign in when not using Capybara as well.
