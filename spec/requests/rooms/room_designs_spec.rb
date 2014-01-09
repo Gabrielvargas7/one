005cd8816d64bfbc23b0ed789818f38517f773af
gabriel <gabriel@mywebroom.com>
2014-01-08 16:37:00 -0800

    search bookmarks fix when is not approval

--- spec/requests/rooms/room_designs_spec.rb
+++ spec/requests/rooms/room_designs_spec.rb
@@ -11,12 +11,15 @@ describe "backbone view rooms", :js => true do
     @user = FactoryGirl.create(:user)
     sign_in @user
     visit room_rooms_path(@user.username)
+    #sleep 50
+    wait_for_ajax
+    #wait_for_dom
   end
 
   subject { page }
 
 
-  describe "design_view", :js => true  do
+  describe "design_view" do
     #before  do
     #  #visit room_rooms_path(@user.username)
     #  @item = Item.first
@@ -26,11 +29,14 @@ describe "backbone view rooms", :js => true do
     #end
 
     #it { page.find('#id').should have_content('loaded')}
-    #it { find('.room_design').should have_content('baz') }
-    #                       #<img class="room_design"
-    it {page.should have_selector('img.room_design')}
+    it { page.find('.room_design').should have_content('baz') }
 
-    #it {page.should have_selector('div.room_design_container_4')}
+    it {page.find('body').should have_content("BOOKMARKS")}
+
+    #                       #<img class="room_design"
+    #it {page.should have_selector('img.room_design')}
+
+    it {page.should have_selector('div.room_design_container_4')}
 
 
     #it "should have all the items" do
