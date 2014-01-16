9c5de7a0611a27117527652f087977e91e12dce9
Dave Riddle <david@joyvuu.com>
2014-01-16 13:57:50 -0800

    Adds comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
+
+  # This action uses the signed_in? method from sessions_helper
+  # to see if the user is signed in.
   def home
     if signed_in?
+      # Question
+      # What is `current_user` (helper method? global variable?)
       redirect_to room_rooms_path(current_user.username)
     end
 
-    @landing_page_view=true
+    # Within what scope are this properties visible? Is it the corresponding view?
+    @landing_page_view = true
     @skip_header = true
     @skip_footer = true
   end
