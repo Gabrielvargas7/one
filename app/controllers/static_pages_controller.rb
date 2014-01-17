a51289f1a7cff276645d5999a1d91ecf9bfd366c
Dave Riddle <david@joyvuu.com>
2014-01-17 08:59:37 -0800

    adds notes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,24 +2,34 @@ class StaticPagesController < ApplicationController
 
   # This action uses the signed_in? method from sessions_helper
   # to see if the user is signed in.
+
+  # It is called when a user navigates to root
   def home
     if signed_in?
       # Question
       # What is `current_user` (helper method? global variable?)
-      redirect_to room_rooms_path(current_user.username)
+      redirect_to room_rooms_path(current_user.username) # could we pass in a flag here that indicated we were in our own room?
+
+
     end
 
-    # Within what scope are this properties visible? Is it the corresponding view?
+    # These are variables available to application.html.erb (any other pages?)
+    # Within application.html.erb, there's a statement that checks to see if this
+    # value is set.
     @landing_page_view = true
     @skip_header = true
     @skip_footer = true
   end
 
+
   def help
   end
+
+
   def about
     @skip_container = true
     @skip_header = false
     @skip_footer = false
   end
+
 end
