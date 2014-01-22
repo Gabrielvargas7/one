26d2617e62fe58470ec4cb935762b1e144f615fd
Sara Neale <sara.n@mywebroom.com>
2014-01-22 14:41:13 -0800

    Add function order to corrsepond to contoller's function order

--- spec/controllers/users_bookmarks_controller_spec.rb
+++ spec/controllers/users_bookmarks_controller_spec.rb
@@ -29,6 +29,162 @@ describe UsersBookmarksController do
 
 
 
+  #/users_bookmarks/json/create_user_bookmark_by_user_id_and_bookmark_id_and_item_id/:user_id/:bookmark_id/:item_id
+  # Form Parameters:
+  #               :position
+  ##***********************************
+  ## rspec test json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id
+  ##***********************************
+
+  describe "POST json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id", tag_json_create:true  do
+
+    before do
+      @item2 = FactoryGirl.create(:item)
+      @bookmarks_category2 = FactoryGirl.create(:bookmarks_category,item_id:@item2.id)
+      @bookmark2 = FactoryGirl.create(:bookmark,bookmarks_category_id:@bookmarks_category2.id)
+      @user_max_position = UsersBookmark.maximum("position")
+
+    end
+
+
+    describe "is regular user" do
+      context "with valid params" do
+
+        it "creates a new user bookmark " do
+          #puts "user id --->"+@user1.id.to_s
+          #puts "user id requested--->"+@user_requested.id.to_s
+          #puts "user max posi " + @user_max_position.to_s
+          expect {
+            post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+          }.to change(UsersBookmark, :count).by(1)
+        end
+
+        it "assigns a newly created user bookmark request as @user_bookmark" do
+
+          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+          assigns(:user_bookmark).should be_a(UsersBookmark)
+          assigns(:user_bookmark).should be_persisted
+
+        end
+
+        it "response should be success" do
+          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+          response.should be_success
+        end
+
+        it "has a 201 status code" do
+          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+          expect(response.status).to eq(201)
+        end
+
+        #{"bookmark_id":101,"created_at":"2013-05-02T14:23:45Z","id":2242,"position":10,"updated_at":"2013-05-02T14:23:45Z","user_id":206}
+        context "return json values " do
+          it "should return friend request in json" do # depend on what you return in action
+            #puts "user id --->"+@user1.id.to_s
+            #puts "user id requested--->"+@user_requested.id.to_s
+            #
+            post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+            body = JSON.parse(response.body)
+            #puts "body ---- > "+body.to_s
+            #puts body["user_friend_request"]["user_id"].to_s
+
+            body["user_id"].should == @user.id
+            body["bookmark_id"].should == @bookmark2.id
+            body["position"].should == @user_max_position+1
+
+
+          end
+        end
+
+      end
+    end
+
+    context "is sign in with other user" do
+      before do
+        @user1 = FactoryGirl.create(:user)
+        sign_in @user1
+      end
+      it "has a 404 status code" do
+        post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+        expect(response.status).to eq(404)
+      end
+    end
+
+
+    context "is sign out user" do
+      before do
+        sign_out
+      end
+      it "has a 404 status code" do
+        post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
+        expect(response.status).to eq(404)
+      end
+    end
+
+
+  end
+
+#***********************************
+  # rspec test  destroy  json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position
+  #***********************************
+
+
+  #/users_bookmarks/json/destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position/:user_id/:bookmark_id/:position
+
+  describe "DELETE json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position",tag_destroy:true do
+    before do
+      @item = FactoryGirl.create(:item)
+      @bookmarks_category = FactoryGirl.create(:bookmarks_category,item_id:@item.id)
+      @bookmark = FactoryGirl.create(:bookmark,bookmarks_category_id:@bookmarks_category.id)
+      @user_max_position = UsersBookmark.maximum("position")
+      @user_bookmark = FactoryGirl.create(:users_bookmark,user_id:@user.id,bookmark_id:@bookmark.id,position:@user_max_position)
+
+    end
+
+    it "deletes friend request" do
+      expect{
+        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
+               user_id: @user.id,
+               bookmark_id:@bookmark.id,
+               position:@user_max_position ,:format => :json
+      }.to change(UsersBookmark,:count).by(-1)
+    end
+
+    context "is sign in with other user" do
+      before do
+        @user1 = FactoryGirl.create(:user)
+        sign_in @user1
+      end
+      it "has a 404 status code" do
+
+        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
+               user_id: @user.id,
+               bookmark_id:@bookmark.id,
+               position:@user_max_position ,:format => :json
+
+        expect(response.status).to eq(404)
+      end
+    end
+
+
+    context "is sign out user" do
+      before do
+        sign_out
+      end
+      it "has a 404 status code" do
+        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
+               user_id: @user.id,
+               bookmark_id:@bookmark.id,
+               position:@user_max_position  ,:format => :json
+
+        expect(response.status).to eq(404)
+      end
+    end
+
+
+  end
+
+
   #***********************************
   # rspec test  #json_index_user_bookmarks_by_user_id
   #***********************************
@@ -119,6 +275,13 @@ describe UsersBookmarksController do
 
 
 
+  end
+
+  #***********************************
+  # rspec test  #json_index_user_bookmarks_by_user_id_by_limit_and_offset
+  #***********************************
+  describe "get json_index_user_bookmarks_by_user_id_by_limit_and_offset" do
+    pending
   end
 
 
@@ -219,101 +382,6 @@ describe UsersBookmarksController do
 
 
 
-
-  #/users_bookmarks/json/create_user_bookmark_by_user_id_and_bookmark_id_and_item_id/:user_id/:bookmark_id/:item_id
-  # Form Parameters:
-  #               :position
-  ##***********************************
-  ## rspec test json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id
-  ##***********************************
-
-  describe "POST json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id", tag_json_create:true  do
-
-    before do
-      @item2 = FactoryGirl.create(:item)
-      @bookmarks_category2 = FactoryGirl.create(:bookmarks_category,item_id:@item2.id)
-      @bookmark2 = FactoryGirl.create(:bookmark,bookmarks_category_id:@bookmarks_category2.id)
-      @user_max_position = UsersBookmark.maximum("position")
-
-    end
-
-
-    describe "is regular user" do
-      context "with valid params" do
-
-        it "creates a new user bookmark " do
-          #puts "user id --->"+@user1.id.to_s
-          #puts "user id requested--->"+@user_requested.id.to_s
-          #puts "user max posi " + @user_max_position.to_s
-          expect {
-            post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-          }.to change(UsersBookmark, :count).by(1)
-        end
-
-        it "assigns a newly created user bookmark request as @user_bookmark" do
-
-          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-          assigns(:user_bookmark).should be_a(UsersBookmark)
-          assigns(:user_bookmark).should be_persisted
-
-        end
-
-        it "response should be success" do
-          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-          response.should be_success
-        end
-
-        it "has a 201 status code" do
-          post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-          expect(response.status).to eq(201)
-        end
-
-        #{"bookmark_id":101,"created_at":"2013-05-02T14:23:45Z","id":2242,"position":10,"updated_at":"2013-05-02T14:23:45Z","user_id":206}
-        context "return json values " do
-          it "should return friend request in json" do # depend on what you return in action
-            #puts "user id --->"+@user1.id.to_s
-            #puts "user id requested--->"+@user_requested.id.to_s
-            #
-            post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-            body = JSON.parse(response.body)
-            #puts "body ---- > "+body.to_s
-            #puts body["user_friend_request"]["user_id"].to_s
-
-            body["user_id"].should == @user.id
-            body["bookmark_id"].should == @bookmark2.id
-            body["position"].should == @user_max_position+1
-
-
-          end
-        end
-
-      end
-    end
-
-    context "is sign in with other user" do
-      before do
-        @user1 = FactoryGirl.create(:user)
-        sign_in @user1
-      end
-      it "has a 404 status code" do
-        post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-        expect(response.status).to eq(404)
-      end
-    end
-
-
-    context "is sign out user" do
-      before do
-        sign_out
-      end
-      it "has a 404 status code" do
-        post :json_create_user_bookmark_by_user_id_and_bookmark_id_and_item_id,user_id:@user.id,bookmark_id:@bookmark2.id,item_id:@item2.id,position:@user_max_position+1,:format => :json
-        expect(response.status).to eq(404)
-      end
-    end
-
-
-  end
 
   #  /users_bookmarks/json/create_user_bookmark_custom_by_user_id/:user_id'
   #  /users_bookmarks/json/create_user_bookmark_custom_by_user_id/1000.json
@@ -476,66 +544,11 @@ describe UsersBookmarksController do
   end
 
 
-
-  #***********************************
-  # rspec test  destroy  json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position
-  #***********************************
-
-
-  #/users_bookmarks/json/destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position/:user_id/:bookmark_id/:position
-
-  describe "DELETE json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position",tag_destroy:true do
-    before do
-      @item = FactoryGirl.create(:item)
-      @bookmarks_category = FactoryGirl.create(:bookmarks_category,item_id:@item.id)
-      @bookmark = FactoryGirl.create(:bookmark,bookmarks_category_id:@bookmarks_category.id)
-      @user_max_position = UsersBookmark.maximum("position")
-      @user_bookmark = FactoryGirl.create(:users_bookmark,user_id:@user.id,bookmark_id:@bookmark.id,position:@user_max_position)
-
-    end
-
-    it "deletes friend request" do
-      expect{
-        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
-               user_id: @user.id,
-               bookmark_id:@bookmark.id,
-               position:@user_max_position ,:format => :json
-      }.to change(UsersBookmark,:count).by(-1)
-    end
-
-    context "is sign in with other user" do
-      before do
-        @user1 = FactoryGirl.create(:user)
-        sign_in @user1
-      end
-      it "has a 404 status code" do
-
-        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
-               user_id: @user.id,
-               bookmark_id:@bookmark.id,
-               position:@user_max_position ,:format => :json
-
-        expect(response.status).to eq(404)
-      end
-    end
-
-
-    context "is sign out user" do
-      before do
-        sign_out
-      end
-      it "has a 404 status code" do
-        delete :json_destroy_user_bookmark_by_user_id_and_by_bookmark_id_and_position,
-               user_id: @user.id,
-               bookmark_id:@bookmark.id,
-               position:@user_max_position  ,:format => :json
-
-        expect(response.status).to eq(404)
-      end
-    end
-
-
+  describe "get json_show_user_bookmark_by_user_id_and_bookmark_id" do
+    pending
   end
+
+  
 
 
   describe "#json/index_bookmarks_with_bookmarks_category_by_item_id_by_limit_and_offset" do
