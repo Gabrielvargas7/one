12293bd0031cedfc0806a25de369166dc2e0e549
gabriel <gabriel@mywebroom.com>
2014-01-27 17:28:53 -0800

    application test header p2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,8 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 #Capybara.javascript_driver = :poltergeist
-Capybara.default_driver = :selenium
+#Capybara.default_driver = :selenium
+Capybara.use_default_driver
 
 ###############
 #Start Test facebook login
